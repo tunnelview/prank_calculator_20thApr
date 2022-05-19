dd55bbb8e23b6e4b946b88c429fb1525de37a865
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-19 14:00:07 +1000

    Grab the display elements and create the function just after the time of click

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll(".btn-area > div");
 
 const buttonsArg = Array.from(buttons);
 
+const display = document.querySelector(".display");
+
 let strToDisplay = ""; //
 
 buttonsArg.map((btn) => {
@@ -11,6 +13,12 @@ buttonsArg.map((btn) => {
     console.log(val);
 
     strToDisplay += val;
+
+    display(strToDisplay);
     console.log(strToDisplay);
   });
 });
+
+const display = (str) => {
+  displayElm.innerText = str;
+};
