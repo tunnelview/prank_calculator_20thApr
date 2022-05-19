6201e91a855bd08a913f875cc9aae143cd73fe47
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-19 13:42:39 +1000

    Attach event to each button

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ document.querySelectorAll(".btn-area > div");
 console.log(buttons);
 
 buttons.map((btn, i) => {
-  console.log(btn);
+  btn.addEventListener("click", () => {
+    const val = btn.innerText;
+
+    //   const val = btn.innerText; to see the inner text of each button on console
+    console.log(btn);
+  });
 });
