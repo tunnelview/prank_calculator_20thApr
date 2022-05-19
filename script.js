e6da88ee66949b37e6f33bb61b6496027dce6042
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-19 13:51:30 +1000

    Add strToDisplay variable to store the clicked value

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const buttons = document.querySelectorAll(".btn-area > div");
 
 const buttonsArg = Array.from(buttons);
 
+let strToDisplay = ""; //
+
 buttonsArg.map((btn) => {
   btn.addEventListener("click", () => {
     const val = btn.innerText;
 
     console.log(val);
+
+    strToDisplay += val;
+    console.log(strToDisplay);
   });
 });
